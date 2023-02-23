09b80b76c4077d7631c06b906ae7e5a0de4fa3a4
lee_crey_116 <solomonboloshe835@gmail.com>
2023-02-23 11:29:31 +0300

    balance

--- db/migrate/20221215064522_create_admin_users.rb
+++ db/migrate/20221215064522_create_admin_users.rb
@@ -9,6 +9,7 @@ class CreateAdminUsers < ActiveRecord::Migration[7.0]
       t.boolean :super_admin, null: false, default: false
       t.integer :products_count, null: false, default: 0
       t.integer :orders_count, null: false, default: 0
+      t.decimal :balance, null: false, default: 0.0
 
       t.timestamps
     end
